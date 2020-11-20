c8ed6ed8e91cf61184df9facf6088e2495724718
Stephen <g00256450@gmit.ie>
2020-11-20 23:38:15 +0000

    Final Commit Lab 2

--- Lab-Programflow/Lab 2/Functions.h
+++ Lab-Programflow/Lab 2/Functions.h
@@ -1,59 +1,84 @@
 #ifndef FUNCTIONS_H_INCLUDED
 #define FUNCTIONS_H_INCLUDED
-float CircleArea;
+using namespace std;
 
-void LabInfo(){
+int program;
+string repeat="Y";//Sets initial condition for running program
+
+void Alarm(){ //Alarm for incorect inputs.
+    //cout<<"\n";
+    cout<<"Would you like to run a different program?\n";
+    cout<<"Enter(Y/N): ";
+    cin>>repeat;
+    cout<<"\n";
+}
+
+//Question 1
+void LabInfo(){ //Displays Information about the Lab.
     cout << "Stephen Higgins (G00256450)\n";
     cout << "Lab #2\n" << endl;
     return;
 }
 
-
-void ReturnTheLargestValue(){
-    int x;
-    int y;
-    cout<<"Enter an integer for first value X: ";
-        cin>> x;
-    cout<<"Enter an integer for second value Y: ";
-        cin>> y;
+//Question 2
+void ReturnTheLargestValue(){//Returns the greater of two values
+int x;//Integer X
+int y;//Integer Y
+cout<<"Enter an integer for first value X: ";//Prompts Question
+cin>> x;//User inputs a value X
+cout<<"Enter an integer for second value Y: ";//Prompts Question
+cin>> y;//User inputs a value Y
     if (x > y){
-        cout << "The largest value is " <<x<<"\n\n";
+        cout << "The largest value is " <<x<<"\n\n";//If X>Y then this condition is true and prints message
     }
     else{
-        cout << "The largest value is " <<y<<"\n\n";
+        cout << "The largest value is " <<y<<"\n\n";//Else Y>X AND Prints this message.
     }
 return;
 }
 
-void Check(){
-    int num;
-    cout<<"Input a integer between 1 and 100"<<endl;
-    cout<<"Integer cannot be 1 or 100!"<<endl;
-    cout<<"Enter your Integer: ";
-    cin>>num;
-    if ((1<num)&&(num<100)){
-        cout<<"The Integer "<<num<<" is between 1 and 100!\n"<<endl;
+//Question 3
+void Check(){//Checks numbers are between 1 and 100 but not equal to 1 or 100
+int num;//Integer
+cout<<"Input a integer between 1 and 100"<<endl;//Propts Message
+cout<<"Integer cannot be 1 or 100!"<<endl;//Propts Message
+cout<<"Enter your Integer: ";//Propts Message
+cin>>num;//User inputs Integer
+    if ((1<num)&&(num<100)){//Check Number is between 1 & 100
+        cout<<"The Integer "<<num<<" is between 1 and 100!\n"<<endl;//If true prints message
     }
-    else if (num<=1){
-        cout <<"The Integer "<<num<<" is less than or equal to 1\n"<<endl;
+    else if (num<=1){//If not ture it check if the values is less than or equal to 1
+        cout <<"The Integer "<<num<<" is less than or equal to 1\n"<<endl;//If this condition is true prints message
     }
-    else{
-        cout <<"The Integer "<<num<<" is greater than or equal to 100\n"<<endl;
+    else{//if neither of above is true then integer is greater than or equal to 100
+        cout <<"The Integer "<<num<<" is greater than or equal to 100\n"<<endl;//If this condition is true prints message
     }
 return;
 }
 
+//Question 5 & 8
 void AreaOfCircle(){
+float answer;//Float Answer
+float radius;//Float Radius
 float PI=3.14159; //Value for pi
-float radius; // Radius Variable
-cout<<"What is the radius of the circle in mm?\n"; //Prints Question to screen
-cin>>radius; // Input value for radius
-float answer=PI * radius * radius; //Calculates the answer for area
-CircleArea=answer;
-cout<<"Area of the circle is: "<<answer<<"mm^2\n\n"; //Prints answer to screen
-return ;
+    if(program==4){//If program 4 is called called executes here
+        cout<<"What is the radius of the circle in m?\n"; //Prints Question to screen
+        cin>>radius; // Input value for radius
+    }
+answer=PI * radius * radius; //Calculates the answer for area
+cout<<"When the radius of the circle is "<<radius<<"m, the area of the circle is "<<answer<<"m^2\n\n";//Prints answer to screen
+    if (program==7){//if program 7 is called executes here
+        int a=0;//Set start point
+        int b=120;//Set finish point
+        for(int initial=a;initial<=b;initial++){//Uses a For loop to iterate from start to finish.
+            answer=PI * initial * initial;//Calculates the answer for area
+            cout<< "When the radius of the circle is "<<initial<<"m, the area of the circle is "<<answer<<"m^2\n\n";//Prints answer to screen
+        }
+    }
+return;
 }
 
+//Question 6
 void AreaOfRectangle(){
 float length;//Value for length
 float width;//Value for width
@@ -66,173 +91,169 @@ cout<<"The area of the rectangle is : "<<Area<< "mm^2\n\n";//Prints answer to sc
 return ;
 }
 
+//Question 7
 void TemperatureAndHumidity(){
-    int Temperature;
-    int Humidity;
-    cout<<"Input Temperature : ";
-    cin>>Temperature;
-    cout<<"Input Humidity : ";
-    cin>>Humidity;
-    cout<<"\n";
-    if (Temperature>=95){
+int Temperature;//Integer Temperature
+int Humidity;//Integer Humidity
+cout<<"Input Temperature : ";//Prints Question to screen
+cin>>Temperature;// Input value for Temperature
+cout<<"Input Humidity : ";//Prints Question to screen
+cin>>Humidity;// Input value for Humidity
+cout<<"\n";
+    if (Temperature>=95){ //If condition is true prints Message
         cout<<"ALARM!!! Temperature is in excess of 95 degrees Celcius!\n"<<endl;
     }
-    else{
+    else{//Else if condition is false prints Message
         cout <<"Temperature is within acceptable limits.\n"<<endl;
     }
-    if (Humidity>=90){
+    if (Humidity>=90){//If condition is true prints Message
         cout<<"ALARM!!! Humidity is in excess of 90 Grams per Cubic Metre!\n"<<endl;
     }
-    else{
+    else{//Else if condition is false prints Message
         cout <<"Humidity is within acceptable limits.\n"<<endl;
     }
 return;
 }
 
-
-void AngularAreaOfCircle(){
-AreaOfCircle();
-for(int x=0;x<=120;x++){
-    cout<< x <<" Degrees of the circle has a area of: "<<(CircleArea/360)*x<<" mm^2\n";
-}
-cout<<"\n";
-return;
-}
+//Question 9
 void TemperatureConversion(){
-    int Program;
-    float Answer;
-    cout<<"Select a Program from the list below.\n\n";
-    cout<<"(1) Fahrenheit to Celcius\n";
-    cout<<"(2) Celcius to Fahrenheit\n";
-    cout<<"What program would you like to run? : ";
-    cin>>Program;
-    cout<<"\n\n";
-    if (Program==1){
-        float Fahrenheit;
-        cout<<"Enter temperature in Degrees Fahrenheit : ";
-        cin>>Fahrenheit;
-        Answer=((Fahrenheit-32)*5/9);
-        cout<<"\n"<<Fahrenheit<<" Degrees Fahrenheit is "<<Answer<<" Degrees Celcius\n\n";
+int Program;//Varible for conversion type
+float Answer;//Float Answer
+cout<<"Select a Program from the list below.\n\n";//Prints question and menu ot screen.
+cout<<"(1) Fahrenheit to Celcius\n";
+cout<<"(2) Celcius to Fahrenheit\n";
+cout<<"What program would you like to run? : ";
+cin>>Program;//User input for conversion type.
+cout<<"\n\n";
+    if (Program==1){ //If user inputs 1, executes this program
+        float Fahrenheit;//Float Fahrenheit
+        cout<<"Enter temperature in Degrees Fahrenheit : ";//Prints question to screen
+        cin>>Fahrenheit;//User input for temperature
+        Answer=((Fahrenheit-32)*5/9);//Equation for conversion
+        cout<<"\n"<<Fahrenheit<<" Degrees Fahrenheit is "<<Answer<<" Degrees Celcius\n\n";//Prints Result too screen
     }
     else if (Program==2){
-        float Celcius;
-        cout<<"Enter temperature in Degrees Celcius : ";
-        cin>>Celcius;
-        Answer=((Celcius*9/5)+32);
-        cout<<"\n"<<Celcius<<" Degrees Celcius is "<<Answer<<" Degrees Fahrenheit\n\n";
+        float Celcius;//Float Celcius
+        cout<<"Enter temperature in Degrees Celcius : ";//Prints question to screen
+        cin>>Celcius;//User input for temperature
+        Answer=((Celcius*9/5)+32);//Equation for conversion
+        cout<<"\n"<<Celcius<<" Degrees Celcius is "<<Answer<<" Degrees Fahrenheit\n\n";//Prints Result too screen
     }
     else{
-        cout<<"Incorrect program selection!\n\n";
+        cout<<"Incorrect program selection!\n\n";//If program input in not 1 or 2 prints message to screen.
     }
 return;
 }
-void PrintEvenNumbers(){
-for(int i=1;i<=1000;i++){
-    if (i%2==0){
-        cout<< i <<"\n";
+
+//Question 10
+void PrintEvenNumbers(){//Prints even numbers to screen between 0 and 100
+    for(int i=1;i<=1000;i++){//For Loop controls iterations
+        if (i%2==0){//Calculation for even numbers
+            cout<< i <<"\n";//Prints answers to screen
+        }
     }
-}
 return;
 }
 
-void FindTheOddNumbers(){
-int x;
-int y;
-    cout<<"Enter the first integer : ";
-        cin>> x;
-    cout<<"Enter the second integer : ";
-        cin>> y;
-if (x%2!=0){
-    cout<<x<<" is a odd number\n";
-}
-else{
-    cout<<x<<" is a even number\n";
-}
-if (y%2!=0){
-    cout<<y<<" is a odd number\n";
-}
-else{
-    cout<<y<<" is a even number\n";
-}
-cout<<"\n";
-}
-
-int FindTheOddNumbers(int a,int b){
-for(int i=a;i<=b;i++){
-    if (i%2!=0){
-        cout<< i <<"\n";
-    }
-}
-return 0;
-}
-
-
-void RandomNumbers(){
-for(int x=1;x<=6;x++){
-   int Random=rand()%10000+1;
-   cout<<Random<<"\n";
-}
+//Question 11
+void FindTheOddNumbers(){//Prints Odd numbers from 2 inputs if present
+int x;//Interger X
+int y;//Interger Y
+cout<<"Enter the first integer : ";//Prints Question to screen
+cin>> x;//User Input for X
+cout<<"Enter the second integer : ";//Prints Question to screen
+cin>> y;//User Input for y
+        if (x%2!=0){//Calculation to detemine if X input is a Odd number
+            cout<<"The number "<< x <<" is a odd number!\n";//Prints to screen if true
+        }
+        if (y%2!=0){//Calculation to detemine if Y input is a Odd number
+            cout<<"The number "<< y <<" is a odd number!\n";//Prints to screen if true
+        }
 cout<<"\n";
 return;
 }
 
+//Question 12
+void FindTheOddNumbers(int theArray [],int sizeofArray){//Iterates through an Array of values and prints the odd values
+    for(int i=0;i<sizeofArray;i++){//For loop to iterate through the array
+        if (theArray[i]%2!=0){//if true prints values to screen
+            cout<<"The number "<< theArray[i] <<" is a odd number!\n";//Prints to screen
+        }
+    }
+cout<<"\n";
+return;
+}
 
+//Question 13
+void RandomNumbers(){//Generates 6 random numbers between 1 and 10000
+for(int x=1;x<=6;x++){//For loop to generate 6 numbers
+   int Random=rand()%10000+1;//Function for generating random numbers
+   cout<<Random<<"\n";//Prints random number to screen
+}
+cout<<"\n";
+return;
+}
+
+//Question 14
 void Exercise14(){
 /* Print the numbers 5 through 9: */
-int i=5;
-while (i < 10){
- cout << i ;
- cout << endl;
- i = i + 1;
-}
+int i=5;//interger to set start point
+    while (i < 10){//while loop to iterate from 5 to 9
+     cout << i ;//Prints interger i each iteration
+     cout << endl;
+     i = i + 1;//Adds 1 to i each iteration
+    }
 cout<<"\n";
 /* Finding the sum 1 + 2 + 3 + ... + 20 */
-i = 1;
-int sum = 0;
-while (i <= 20){
- sum = sum + i;
- i = i + 1;
-}
-cout << "The sum = " << sum<<"\n\n";
+i = 1;//interger to set start point
+int sum = 0;//interger to set result
+    while (i <= 20){//while loop to iterate from 5 to 9
+     sum = sum + i;//Accumelator for sum
+     i = i + 1;//Adds 1 to i each iteration
+    }
+cout << "The sum = " << sum<<"\n\n";//Prints resulting sum to screen
 
 /* Average a list of grades terminated by -1 */
-sum = 0;
-int count = 0;
-int grade;
+sum = 0;//interger to set result
+int count = 0;//interger to set count
+int grade;//integer for grade
 cout << "Enter grade (-1 to end): "; // prompt user for grade
 cin >> grade; // read grade
-while (grade != -1){
- sum = sum + grade;
- count = count + 1;
- /* Get next grade */
- cout << "Enter grade (-1 to end): ";
- cin >> grade;
-}
-if (count > 0){
- cout << "Average is " << (double) sum / count<<"\n";
-}
+    while (grade != -1){//while loop to iterates until user inserts -1
+     sum = sum + grade;//Accumelator for sum
+     count = count + 1;//Counts the number of grades inputted
+     /* Get next grade */
+     cout << "Enter grade (-1 to end): ";// prompt user for grade
+     cin >> grade;//User inputs grade or -1
+    }
+    if (count > 0){//If the count is greater than 0, calculats the average
+     cout << "Average is " << (double) sum / count<<"\n";//Prints Average to screen
+    }
 return;
 }
+
+//Question 15
 void Exercise15(){
-    int i=1;
-    while(i<=10){
+int i=1;
+    while(i<=10){//While Loop for 10 iterations prints "Finished"
         cout<<"Finished\n";
         i=i+1;
     }
 cout<<"\n";
-for(int a=1;a<=10;a++){
+    for(int a=1;a<=10;a++){//For Loop for 10 iterations prints "Finished"
        cout<<"Finished\n";
     }
 cout<<"\n";
 i=1;
-do{
-    cout<<"Finished\n";
-    i=i+1;
-}
+    do{
+        cout<<"Finished\n";//DO Loop for 10 iterations prints "Finished"
+        i=i+1;
+    }
     while (i<=10);
 
 cout<<"\n";
 
 }
+
 
 #endif // FUNCTIONS_H_INCLUDED
--- Lab-Programflow/Lab 2/main.cpp
+++ Lab-Programflow/Lab 2/main.cpp
@@ -1,110 +1,105 @@
 #include <iostream>
-using namespace std;
 #include "Functions.h"
 
-int program;
+using namespace std;
+
 
 int main(){
-string repeat="Y";
 
-while ((repeat=="Y")or(repeat=="y")){
-    repeat="N";
-cout<<"What program would you like to run?\n";
-cout<<"\n";
-cout<<"(1)  Information about the Program\n";
-cout<<"(2)  Check the largest of Two Inegers\n";
-cout<<"(3)  Check an interger is between 1 and 100\n";
-cout<<"(4)  Calculate the area of a circle\n";
-cout<<"(5)  Calculate the area of a Rectangle\n";
-cout<<"(6)  Check Temperature and Humidity is within Limits\n";
-cout<<"(7)  Section area of a Circle from 0 to 120 degrees\n";
-cout<<"(8)  Temperature Conversion\n";
-cout<<"(9)  Print all even numbers from 1-1000\n";
-cout<<"(10) Find the odd numbers\n";
-cout<<"(11) OverLoaded Function\n";
-cout<<"(12) Generate 6 random Numbers\n";
-cout<<"(13) Lab 2 Exercise 14\n";
-cout<<"(14) Lab 2 Exercise 15\n";
+    while ((repeat=="Y")or(repeat=="y")){ //Starts a loop for re-entering the menu without exiting
+    cout<<"What program would you like to run?\n";//Prompts Question
+    cout<<"\n";//New Line
+    cout<<"(1)  Q1  Information about the Program\n";//Program Options 1-14
+    cout<<"(2)  Q2  Check the largest of Two Inegers\n";
+    cout<<"(3)  Q3  Check an interger is between 1 and 100\n";
+    cout<<"(4)  Q5  Calculate the area of a circle\n";
+    cout<<"(5)  Q6  Calculate the area of a Rectangle\n";
+    cout<<"(6)  Q7  Check Temperature and Humidity are set within Limits\n";
+    cout<<"(7)  Q8  Area of a Circle from 0 to 120 meters\n";
+    cout<<"(8)  Q9  Temperature Conversion\n";
+    cout<<"(9)  Q10 Print all even numbers from 1-1000\n";
+    cout<<"(10) Q11 Find the odd numbers\n";
+    cout<<"(11) Q12 OverLoaded Function\n";
+    cout<<"(12) Q13 Generate 6 random Numbers\n";
+    cout<<"(13) Q14 Lab 2 Exercise 14\n";
+    cout<<"(14) Q15 Lab 2 Exercise 15\n";
+    cout<<"\n";
+    cout<<"Enter the program number you wish to run (1-14): "; //Prompts user to input selected program number
+    cin>>program;//User Inputs Number
+    cout<<"\n";
 
-cout<<"\n";
-cout<<"Enter the program number you wish to run (1-14): ";
-cin>>program;
-cout<<"\n";
+        if ((1<=program)&&(program<=14)){// If program is within set limits the program will be selected
+            switch (program){            // else the program will enter alarm state.
+                case 1:
+                    LabInfo();
+                break;
+                case 2:
+                    ReturnTheLargestValue();
+                break;
+                case 3:
+                    Check();
+                break;
+                case 4:
+                    AreaOfCircle();
+                break;
+                case 5:
+                    AreaOfRectangle();
+                break;
+                case 6:
+                    TemperatureAndHumidity();
+                break;
+                case 7:
+                    AreaOfCircle();
+                break;
+                case 8:
+                    TemperatureConversion();
+                break;
+                case 9:
+                    PrintEvenNumbers();
+                break;
+                case 10:
+                    FindTheOddNumbers();
+                break;
+                case 12:
+                    RandomNumbers();
+                break;
+                case 13:
+                    Exercise14();
+                break;
+                case 14:
+                    Exercise15();
+                break;
+                    }
+                if (program==11){                              //Program 11 would not work under switch command
+                    int sizeofArray;                           //Was required to insert underneath as a if.
+                    cout<<"Enter the size of the array : ";
+                    cin>> sizeofArray;//User Inserts the numbr of elements
+                    int theArray[sizeofArray];
+                        for (int i=0;i<sizeofArray;i++){//Iterates through elements of array and asks user to input value for each.
+                        cout<<"Enter element "<< i+1 <<" of the array : ";
+                        cin>> theArray[i];//User input for elements
+                        }
+                    FindTheOddNumbers(theArray,sizeofArray);
+                }
+            }
 
-if ((1<=program)&&(program<=14)){
-    switch (program){
-        case 1:
-        LabInfo();
-        break;
-        case 2:
-        ReturnTheLargestValue();
-        break;
-        case 3:
-        Check();
-        break;
-        case 4:
-        AreaOfCircle();
-        break;
-        case 5:
-        AreaOfRectangle();
-        break;
-        case 6:
-        TemperatureAndHumidity();
-        break;
-        case 7:
-        AngularAreaOfCircle();
-        break;
-        case 8:
-        TemperatureConversion();
-        break;
-        case 9:
-        PrintEvenNumbers();
-        break;
-        case 10:
-        FindTheOddNumbers();
-        break;
-        case 11:
-        int a,b;
-        cout<<"Enter start interger (A) : ";
-        cin>> a;
-        cout<<"Enter finish integer (B) : ";
-        cin>> b;
-        cout<<"\n";
-        FindTheOddNumbers(a,b);
-        break;
-        case 12:
-        RandomNumbers();
-        break;
-        case 13:
-        Exercise14();
-        break;
-        case 14:
-        Exercise15();
-        break;
+        else{
+        cout<<"Input is not included in scope!\n\nPlease input a program number listed.\n\n";// If a value outside the set values is input by
+        }                                                                                    // the user the program prompts a message to the user.
+
+    Alarm();
+
+        while ((repeat!="y")&&(repeat!="n")&&(repeat!="Y")&&(repeat!="N")){//Unless the required Upper or Lower case "N" or "Y" is
+            cout<<"Invalid Entry!\n";                                      // input by the user the program will loop here.
+            Alarm();
+        }
+
     }
-}
-
-else{
-cout<<"Input is not included in scope!\n\nPlease input a program number listed.\n\n";
-}
-
-cout<<"\n\n";
-
-cout<<"Would you like to run a different program?\n";
-cout<<"Enter(Y/N): ";
-cin>>repeat;
-cout<<"\n\n";
-
-while ((repeat!="y")&&(repeat!="n")&&(repeat!="Y")&&(repeat!="N")){
-cout<<"Invalid Entry!\n\n";
-cout<<"Would you like to run a different program?\n\n";
-cout<<"Enter(Y/N):";
-cin>>repeat;
-cout<<"\n\n";
-}
-
-}
-cout<<"Goodbye!\n\n";
+cout<<"Goodbye!\n\n";// Prints Goodbye when terminating program.
 return 0;
 }
+
+
+
+
 
