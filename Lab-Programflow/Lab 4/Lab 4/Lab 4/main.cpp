1884834aa069e6fd06acd0a95e9a47bec88eebe6
Stephen <g00256450@gmit.ie>
2021-02-12 12:11:31 +0000

    Third Commit Lab 4

--- Lab-Programflow/Lab 4/Lab 4/Lab 4/main.cpp
+++ Lab-Programflow/Lab 4/Lab 4/Lab 4/main.cpp
@@ -8,20 +8,27 @@ class LabInfo{          // Create a Class Called LabInfo
     public:             // Make Data Public
         string Name;    // String to Capture Name
         string LabID;   // String to Capture the Lab ID
+
+        void SetDate(int valDay,int valMonth,int valYear){                      // +++Function For Setting Private Info+++
+            Day=valDay;                                                         // This Function can access Data that cant
+            Month=valMonth;                                                     // be accessed outside the LabInfo Class
+            Year=valYear;
+        }
+
+        void printInfo(LabInfo Info){                                           // +++Function For Printing Lab Infomation+++
+            cout<<Info.Name<<endl;                                              // Print Name
+            cout<<Info.LabID<<endl;                                             // Print Lab Information
+            cout<<"Date: "<<Info.Day<<"/"<<Info.Month<<"/"<<Info.Year<<endl;    // Print Date
+        }
+
+    private:            // Make Data Private
         int Day;        // Integer to Capture Day
         int Month;      // Integer to Capture Month
         int Year;       // Integer to Capture Year
 
-        void printInfo(LabInfo Info){
-            cout<<Info.Name<<endl;
-            cout<<Info.LabID<<endl;
-            cout<<"Date: "<<Info.Day<<"/"<<Info.Month<<"/"<<Info.Year<<endl;
-        }
+
 };
 
-
-
-//void Labinfo();
 
 int main(){
 
@@ -29,19 +36,14 @@ int main(){
 
     Info.Name="Stephen Higgins (G00256450)";
     Info.LabID="Lab Using Classes";
-    Info.Day=12;
-    Info.Month=2;
-    Info.Year=2021;
+    Info.SetDate(12,2,2021);
+
 
     Info.printInfo(Info);
 
     return 0;
 }
 
-//void Labinfo(){
-    //cout << "Stephen Higgins (G00256450) \n";
-    //cout << "Lab Using Classes" << endl;
-//}
 
 
 
