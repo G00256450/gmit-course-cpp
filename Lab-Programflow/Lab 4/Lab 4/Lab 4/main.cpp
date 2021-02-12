8b70312a8fffd8a9ee95d57634e2f936fa44ed64
Stephen <g00256450@gmit.ie>
2021-02-12 11:51:57 +0000

    First Commit Lab 4

---
+++ Lab-Programflow/Lab 4/Lab 4/Lab 4/main.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+
+using namespace std;
+
+//Class for storing Lab Information
+
+class LabInfo{          // Create a Class Called LabInfo
+    public:             // Make Data Public
+        string Name;    // String to Capture Name
+        string LabID;   // String to Capture the Lab ID
+        int Day;        // Integer to Capture Day
+        int Month;      // Integer to Capture Month
+        int Year;       // Integer to Capture Year
+};
+
+void printInfo(LabInfo Info){
+            cout<<Info.Name<<endl;
+            cout<<Info.LabID<<endl;
+            cout<<"Date: "<<Info.Day<<"/"<<Info.Month<<"/"<<Info.Year<<endl;
+        }
+
+//void Labinfo();
+
+int main(){
+
+    LabInfo Info;
+
+    Info.Name="Stephen Higgins (G00256450)";
+    Info.LabID="Lab Using Classes";
+    Info.Day=12;
+    Info.Month=2;
+    Info.Year=2021;
+
+    printInfo(Info);
+
+    return 0;
+}
+
+//void Labinfo(){
+    //cout << "Stephen Higgins (G00256450) \n";
+    //cout << "Lab Using Classes" << endl;
+//}
+
+
+
